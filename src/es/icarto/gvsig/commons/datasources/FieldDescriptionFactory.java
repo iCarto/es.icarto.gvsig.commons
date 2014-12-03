6496086f7cc5d29b84c8c091a8f38f9efee84dd7
Francisco Puga <fpuga@icarto.es>
2014-12-03 18:50:00 +0100

    New utility class: FieldDescriptionFactory

---
+++ src/es/icarto/gvsig/commons/datasources/FieldDescriptionFactory.java
@@ -0,0 +1,99 @@
+package es.icarto.gvsig.commons.datasources;
+
+import java.sql.Types;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.iver.cit.gvsig.fmap.drivers.FieldDescription;
+
+public class FieldDescriptionFactory {
+
+    private int stringLength = 100;
+    private int numericLength = 20;
+    private int decimalCount = 6;
+
+    private final List<FieldDescription> fields = new ArrayList<FieldDescription>();
+
+    public void setDefaultStringLength(int defaultStringLength) {
+	this.stringLength = defaultStringLength;
+    }
+
+    public void setDefaultNumericLength(int defaultNumericLength) {
+	this.numericLength = defaultNumericLength;
+    }
+
+    public void setDefaultDecimalCount(int defaultDecimalCount) {
+	this.decimalCount = defaultDecimalCount;
+    }
+
+    public void addInteger(String name) {
+	FieldDescription fd = getInteger(name);
+	fields.add(fd);
+    }
+
+    public FieldDescription getInteger(String name) {
+	FieldDescription fd = new FieldDescription();
+	fd.setFieldName(name);
+	fd.setFieldLength(numericLength);
+	fd.setFieldDecimalCount(0);
+	fd.setFieldType(Types.INTEGER);
+	return fd;
+    }
+
+    public void addDouble(String name) {
+	FieldDescription fd = getDouble(name);
+	fields.add(fd);
+    }
+
+    public FieldDescription getDouble(String name) {
+	FieldDescription fd = new FieldDescription();
+	fd.setFieldName(name);
+	fd.setFieldLength(numericLength);
+	fd.setFieldDecimalCount(decimalCount);
+	fd.setFieldType(Types.DOUBLE);
+	return fd;
+    }
+
+    public void addString(String name) {
+	FieldDescription fd = getString(name);
+	fields.add(fd);
+    }
+
+    public FieldDescription getString(String name) {
+	FieldDescription fd = new FieldDescription();
+	fd.setFieldName(name);
+	fd.setFieldLength(stringLength);
+	fd.setFieldDecimalCount(0);
+	fd.setFieldType(Types.VARCHAR);
+	return fd;
+    }
+
+    public void addDate(String name) {
+	FieldDescription fd = getDate(name);
+	fields.add(fd);
+    }
+
+    public FieldDescription getDate(String name) {
+	FieldDescription fd = new FieldDescription();
+	fd.setFieldName(name);
+	fd.setFieldType(Types.DATE);
+	return fd;
+    }
+
+    public void addBoolean(String name) {
+	FieldDescription fd = getBoolean(name);
+	fields.add(fd);
+    }
+
+    public FieldDescription getBoolean(String name) {
+	FieldDescription fd = new FieldDescription();
+	fd.setFieldName(name);
+	fd.setFieldType(Types.BIT);
+	return fd;
+    }
+
+    public FieldDescription[] getFields() {
+	return fields.toArray(new FieldDescription[0]);
+    }
+
+}
