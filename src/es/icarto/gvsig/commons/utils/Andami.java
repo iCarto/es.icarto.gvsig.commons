413715214d5c412b4a1c91ef8c896252a621f2bc
Francisco Puga <fpuga@icarto.es>
2015-03-26 17:36:52 +0100

    New utility method to create view windows

---
+++ src/es/icarto/gvsig/commons/utils/Andami.java
@@ -0,0 +1,43 @@
+package es.icarto.gvsig.commons.utils;
+
+import com.iver.andami.PluginServices;
+import com.iver.andami.ui.mdiManager.IWindow;
+import com.iver.cit.gvsig.ProjectExtension;
+import com.iver.cit.gvsig.project.Project;
+import com.iver.cit.gvsig.project.documents.ProjectDocument;
+import com.iver.cit.gvsig.project.documents.ProjectDocumentFactory;
+import com.iver.cit.gvsig.project.documents.view.ProjectViewFactory;
+import com.iver.cit.gvsig.project.documents.view.gui.View;
+
+public class Andami {
+
+    /**
+     * If the active window is a View returns it, if not creates a new one, adds
+     * it to the project and returns it
+     * 
+     */
+    public static View createViewIfNeeded(String viewName) {
+
+	// TODO: fpuga: Check what happens when exists a view in the project but
+	// is not active
+	IWindow iWindow = PluginServices.getMDIManager().getActiveWindow();
+	View view = null;
+
+	if (iWindow instanceof View) {
+	    view = (View) iWindow;
+	} else {
+
+	    Project project = ((ProjectExtension) PluginServices
+		    .getExtension(ProjectExtension.class)).getProject();
+	    ProjectDocumentFactory viewFactory = Project
+		    .getProjectDocumentFactory(ProjectViewFactory.registerName);
+	    ProjectDocument projectDocument = viewFactory.create(project);
+	    projectDocument.setName(viewName);
+	    project.addDocument(projectDocument);
+	    view = (View) projectDocument.createWindow();
+	    view.getWindowInfo().setMaximized(true);
+	    PluginServices.getMDIManager().addWindow(view);
+	}
+	return view;
+    }
+}
