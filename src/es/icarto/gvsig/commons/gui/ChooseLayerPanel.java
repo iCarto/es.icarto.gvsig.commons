fe655b2b781994784324e82537d6ca41eb94c1e6
Francisco Puga <fpuga@icarto.es>
2015-03-30 19:24:48 +0200

    Adds a panel with a combobox and a label to select layers from the toc

---
+++ src/es/icarto/gvsig/commons/gui/ChooseLayerPanel.java
@@ -0,0 +1,160 @@
+package es.icarto.gvsig.commons.gui;
+
+import java.util.List;
+
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import com.iver.andami.PluginServices;
+import com.iver.andami.ui.mdiManager.IWindow;
+import com.iver.cit.gvsig.fmap.layers.FLayer;
+import com.iver.cit.gvsig.project.documents.view.gui.View;
+
+@SuppressWarnings("serial")
+/**
+ * The api of this class will change in the future. Comment that you are going
+ * to use
+ */
+@Deprecated
+public class ChooseLayerPanel<T extends FLayer> extends JPanel {
+
+    public enum Orientation {
+	VERTICAL, HORIZONTAL, PLACEHOLDER, NONE
+    }
+
+    private final LayerItem EMPTY = new LayerItem(null);
+
+    private final class LayerItem {
+
+	/**
+	 * FLayer classes overrides toString method for some opaque reason, so
+	 * we can not put it directly on a combo This is a wrapper
+	 */
+	private final T layer;
+	private final String name;
+
+	public LayerItem(T layer) {
+	    this.layer = layer;
+	    this.name = layer != null ? layer.getName() : "";
+	}
+
+	public T getLayer() {
+	    return this.layer;
+	}
+
+	@Override
+	public String toString() {
+	    return this.name;
+	}
+    }
+
+    private final JComboBox layerCombo;
+
+    private final Class<T> clasz;
+
+    private static final String prototypeDisplayValue = "XXXXXXXXXXXXXXXXXXXX";
+
+    /**
+     * @text the text to use for the label. If null or empty Orientation.NONE
+     *       will be assumed. The text will be automatically i18n through
+     *       PluginServices
+     * 
+     * @param orientation
+     *            if the label of the combo should be in the left of the combo
+     *            (HORIZONTAL), over the combo (VERTICAL) or instead of use a
+     *            label a placeholder should be used in the combo itself
+     *            (PLACEHOLDER), or even with no label at all (NONE)
+     */
+    public ChooseLayerPanel(String text, Orientation orientation, Class<T> clasz) {
+
+	this.clasz = clasz;
+
+	JLabel label = null;
+	if ((text == null) || text.isEmpty()) {
+	    orientation = Orientation.NONE;
+	} else {
+	    label = new JLabel(PluginServices.getText(this, text));
+	}
+
+	layerCombo = new JComboBox();
+	layerCombo.setPrototypeDisplayValue(prototypeDisplayValue);
+
+	switch (orientation) {
+	case HORIZONTAL:
+	    this.add(label);
+	    this.add(layerCombo);
+	    break;
+
+	default:
+	    throw new AssertionError("Not implemented jet");
+	}
+
+    }
+
+    /*
+     * Preselects in the combo the first layer active of type T and returns it
+     * or null if there is no active layer
+     */
+    public T preselectFirstActive() {
+	for (int i = 0; i < layerCombo.getItemCount(); i++) {
+	    LayerItem itemAt = (LayerItem) layerCombo.getItemAt(i);
+
+	    T l = itemAt.getLayer();
+	    if ((l != null) && l.isActive()) {
+		layerCombo.setSelectedIndex(i);
+		return l;
+	    }
+	}
+	return null;
+    }
+
+    /**
+     * Adds an empty first item in the combo and preselects it [preselect] is
+     * true Take care with the order in which preselectFirstActive and this
+     * method is used
+     * 
+     * Should be called before populateFrom
+     * 
+     * @param preselect
+     */
+    public void addEmptyFirst(boolean preselect) {
+	layerCombo.addItem(EMPTY);
+    }
+
+    /**
+     * Returns the selected layer or null if none is selected
+     */
+    public T getSelected() {
+	LayerItem sel = (LayerItem) layerCombo.getSelectedItem();
+	return sel.getLayer();
+    }
+
+    /**
+     * Returns a list with the layers used to fill the combo
+     */
+    public List<T> populateFrom(View view) {
+	TOCLayerManager toc = new TOCLayerManager(view.getMapControl());
+	List<T> allLayers = toc.getAllLayers(clasz);
+	for (T t : allLayers) {
+	    layerCombo.addItem(new LayerItem(t));
+	}
+	return allLayers;
+    }
+
+    public List<T> populateFromActiveView() {
+	View view = getView();
+	if (view != null) {
+	    return populateFrom(view);
+	}
+	return null;
+    }
+
+    private View getView() {
+	IWindow iWindow = PluginServices.getMDIManager().getActiveWindow();
+	if (iWindow instanceof View) {
+	    return (View) iWindow;
+	}
+	return null;
+    }
+}
---
+++ src/es/icarto/gvsig/commons/gui/TOCLayerManager.java
@@ -0,0 +1,116 @@
+package es.icarto.gvsig.commons.gui;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.iver.cit.gvsig.fmap.MapControl;
+import com.iver.cit.gvsig.fmap.layers.FLayer;
+import com.iver.cit.gvsig.fmap.layers.FLayers;
+import com.iver.cit.gvsig.fmap.layers.FLyrVect;
+
+/**
+ * The api of this class will change in the future. Comment that you are going
+ * to use
+ */
+@Deprecated
+public class TOCLayerManager {
+
+    private MapControl mapControl = null;
+
+    @SuppressWarnings("unused")
+    private TOCLayerManager() {
+	throw new AssertionError();
+    }
+
+    protected TOCLayerManager(MapControl mapControl) {
+	this.mapControl = mapControl;
+    }
+
+    public FLyrVect getActiveLayer() {
+	if (mapControl != null) {
+	    FLayer[] activeLayers = mapControl.getMapContext().getLayers()
+		    .getActives();
+	    for (FLayer layer : activeLayers) {
+		if (isFLyrVect(layer)) {
+		    return (FLyrVect) layer;
+		}
+	    }
+	}
+	return null;
+    }
+
+    public FLyrVect[] getActiveLayers() {
+	List<FLyrVect> layers = new ArrayList<FLyrVect>();
+	if (mapControl != null) {
+	    FLayer[] activeLayers = mapControl.getMapContext().getLayers()
+		    .getActives();
+	    for (FLayer layer : activeLayers) {
+		if (isFLyrVect(layer)) {
+		    layers.add((FLyrVect) layer);
+		}
+	    }
+	}
+	return layers.toArray(new FLyrVect[0]);
+    }
+
+    public FLyrVect[] getVisibleLayers() {
+	List<FLyrVect> layers = new ArrayList<FLyrVect>();
+	if (mapControl != null) {
+	    FLayer[] activeLayers = mapControl.getMapContext().getLayers()
+		    .getVisibles();
+	    for (FLayer layer : activeLayers) {
+		if (isFLyrVect(layer)) {
+		    layers.add((FLyrVect) layer);
+		}
+	    }
+	}
+	return layers.toArray(new FLyrVect[0]);
+    }
+
+    public String getNameOfActiveLayer() {
+	FLyrVect layer = getActiveLayer();
+	if (layer != null) {
+	    return layer.getName();
+	}
+	return null;
+    }
+
+    public <T extends FLayer> List<T> getAllLayers(Class<T> clasz) {
+	List<T> layers = new ArrayList<T>();
+	layers.addAll(getInnerLayers(mapControl.getMapContext().getLayers(),
+		clasz));
+	return layers;
+    }
+
+    protected <T extends FLayer> List<T> getInnerLayers(FLayers layerGroup,
+	    Class<T> clasz) {
+	List<T> layers = new ArrayList<T>();
+	// FLayers returns the layers in inverse order of how the appear in the
+	// TOC
+	for (int i = layerGroup.getLayersCount() - 1; i >= 0; i--) {
+	    FLayer layer = layerGroup.getLayer(i);
+	    if (isFLayers(layer)) {
+		List<T> innerLayers = getInnerLayers((FLayers) layer, clasz);
+		layers.addAll(innerLayers);
+		continue;
+	    }
+
+	    if (clasz.isAssignableFrom(layer.getClass())) {
+		layers.add((T) layer);
+	    }
+	}
+	return layers;
+    }
+
+    public boolean hasName(FLayer layer, String layerName) {
+	return layer.getName().equalsIgnoreCase(layerName);
+    }
+
+    public boolean isFLyrVect(FLayer layer) {
+	return layer instanceof FLyrVect;
+    }
+
+    public boolean isFLayers(final FLayer layer) {
+	return layer instanceof FLayers;
+    }
+}
