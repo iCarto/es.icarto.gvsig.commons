f46ccc43b7a94e8e629b44c9325c3d8e62b41bb7
Francisco Puga <fpuga@icarto.es>
2015-03-30 19:25:20 +0200

    Adds a panel that builds chained combobox from a tree model

---
+++ src-test/es/icarto/gvsig/commons/gui/ChainedComboPanelTest.java
@@ -0,0 +1,84 @@
+package es.icarto.gvsig.commons.gui;
+
+import java.awt.BorderLayout;
+import java.net.URL;
+
+import javax.swing.JFrame;
+import javax.swing.tree.DefaultMutableTreeNode;
+
+public class ChainedComboPanelTest {
+
+    public static final class BookInfo {
+	public String bookName;
+	public URL bookURL;
+
+	public BookInfo(String book, String filename) {
+	    bookName = book;
+	    bookURL = getClass().getResource(filename);
+	    if (bookURL == null) {
+		// System.err.println("Couldn't find file: " + filename);
+	    }
+	}
+
+	@Override
+	public String toString() {
+	    return bookName;
+	}
+    }
+
+    public static void main(String[] args) {
+	JFrame f = new JFrame("Dual List Box Tester");
+	f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	DefaultMutableTreeNode top = new DefaultMutableTreeNode(
+		"The Java Series");
+	createNodes(top);
+
+	ChainedComboPanel dialog = new ChainedComboPanel(top);
+
+	f.getContentPane().add(dialog, BorderLayout.CENTER);
+	f.pack();
+	f.setVisible(true);
+    }
+
+    private static void createNodes(DefaultMutableTreeNode top) {
+	DefaultMutableTreeNode category = null;
+	DefaultMutableTreeNode book = null;
+
+	category = new DefaultMutableTreeNode("Books for Java Programmers");
+	top.add(category);
+
+	// original Tutorial
+	book = new DefaultMutableTreeNode(new BookInfo(
+		"The Java Tutorial: A Short Course on the Basics",
+		"tutorial.html"));
+	category.add(book);
+
+	// Tutorial Continued
+	book = new DefaultMutableTreeNode(new BookInfo(
+		"The Java Tutorial Continued: The Rest of the JDK",
+		"tutorialcont.html"));
+	category.add(book);
+
+	// Swing Tutorial
+	book = new DefaultMutableTreeNode(new BookInfo(
+		"The Swing Tutorial: A Guide to Constructing GUIs",
+		"swingtutorial.html"));
+	category.add(book);
+
+	// ...add more books for programmers...
+
+	category = new DefaultMutableTreeNode("Books for Java Implementers");
+	top.add(category);
+
+	// VM
+	book = new DefaultMutableTreeNode(new BookInfo(
+		"The Java Virtual Machine Specification", "vm.html"));
+	category.add(book);
+
+	// Language Spec
+	book = new DefaultMutableTreeNode(new BookInfo(
+		"The Java Language Specification", "jls.html"));
+	category.add(book);
+    }
+
+}
---
+++ src/es/icarto/gvsig/commons/gui/ChainedComboPanel.java
@@ -0,0 +1,88 @@
+package es.icarto.gvsig.commons.gui;
+
+import java.awt.Component;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import javax.swing.ComboBoxModel;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.tree.DefaultMutableTreeNode;
+
+import net.miginfocom.swing.MigLayout;
+
+/**
+ * The api of this class will change in the future. Comment that you are going
+ * to use
+ */
+@Deprecated
+@SuppressWarnings("serial")
+public class ChainedComboPanel extends JPanel implements ItemListener {
+
+    private static final String prototypeDisplayValue = "XXXXXXXXXXXXXXXXXXXX";
+
+    private final List<JComboBox> l = new ArrayList<JComboBox>();
+
+    public ChainedComboPanel(DefaultMutableTreeNode top) {
+	super(new MigLayout());
+
+	DefaultMutableTreeNode node = top;
+	List<String> labels = (List<String>) top.getUserObject();
+
+	int childCount = node.getChildCount();
+	while (childCount > 0) {
+	    Object[] values = Collections.list(node.children()).toArray();
+	    JComboBox c = new JComboBox(values);
+	    c.setPrototypeDisplayValue(prototypeDisplayValue);
+	    l.add(c);
+	    c.addItemListener(this);
+	    this.add(new JLabel(labels.remove(0)));
+	    this.add(c, "wrap");
+	    node = (DefaultMutableTreeNode) node.getFirstChild();
+	    childCount = node.getChildCount();
+	}
+    }
+
+    @Override
+    public void itemStateChanged(ItemEvent event) {
+	if (event.getStateChange() == ItemEvent.SELECTED) {
+	    Object combo = event.getSource();
+	    int indexOf = l.indexOf(combo);
+	    if (indexOf < l.size() - 1) {
+		DefaultMutableTreeNode node = (DefaultMutableTreeNode) event
+			.getItem();
+		JComboBox nextCombo = l.get(indexOf + 1);
+		Object[] values = Collections.list(node.children()).toArray();
+		ComboBoxModel aModel = new DefaultComboBoxModel(values);
+		nextCombo.setModel(aModel);
+	    }
+	}
+    }
+
+    public Object getSelected() {
+	JComboBox jComboBox = l.get(l.size() - 1);
+	DefaultMutableTreeNode item = (DefaultMutableTreeNode) jComboBox
+		.getSelectedItem();
+	return item.getUserObject();
+    }
+
+    public List<Object> getAllSelected() {
+	List<Object> selected = new ArrayList<Object>(l.size());
+	for (JComboBox j : l) {
+	    DefaultMutableTreeNode item = (DefaultMutableTreeNode) j
+		    .getSelectedItem();
+	    selected.add(item.getUserObject());
+	}
+	return selected;
+    }
+
+    public Component getDefaultFocusComponent() {
+	return l.get(0);
+    }
+
+}
