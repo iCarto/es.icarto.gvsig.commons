1bbf6082357af329d5399b6d48f771aaed584d0c
Francisco Puga <fpuga@icarto.es>
2015-05-11 20:10:24 +0200

    The createViewIfNeeded now admits a crs parameter

--- src/es/icarto/gvsig/commons/utils/Andami.java
+++ src/es/icarto/gvsig/commons/utils/Andami.java
@@ -3,6 +3,7 @@ package es.icarto.gvsig.commons.utils;
 import com.iver.andami.PluginServices;
 import com.iver.andami.ui.mdiManager.IWindow;
 import com.iver.cit.gvsig.ProjectExtension;
+import com.iver.cit.gvsig.fmap.crs.CRSFactory;
 import com.iver.cit.gvsig.project.Project;
 import com.iver.cit.gvsig.project.documents.ProjectDocument;
 import com.iver.cit.gvsig.project.documents.ProjectDocumentFactory;
@@ -11,12 +12,16 @@ import com.iver.cit.gvsig.project.documents.view.gui.View;
 
 public class Andami {
 
+    public static View createViewIfNeeded(String viewName) {
+	return createViewIfNeeded(viewName);
+    }
+
     /**
      * If the active window is a View returns it, if not creates a new one, adds
      * it to the project and returns it
-     * 
+     *
      */
-    public static View createViewIfNeeded(String viewName) {
+    public static View createViewIfNeeded(String viewName, String crs) {
 
 	// TODO: fpuga: Check what happens when exists a view in the project but
 	// is not active
@@ -36,6 +41,9 @@ public class Andami {
 	    project.addDocument(projectDocument);
 	    view = (View) projectDocument.createWindow();
 	    view.getWindowInfo().setMaximized(true);
+	    if ((crs != null) && !crs.isEmpty()) {
+		view.setProjection(CRSFactory.getCRS(crs));
+	    }
 	    PluginServices.getMDIManager().addWindow(view);
 	}
 	return view;
