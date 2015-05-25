b61fce6dd6f16f75844a47ea804878ed51f27e9d
Francisco Puga <fpuga@icarto.es>
2015-05-25 22:02:50 +0200

    New utility method to copy files

--- src/es/icarto/gvsig/commons/utils/FileUtils.java
+++ src/es/icarto/gvsig/commons/utils/FileUtils.java
@@ -1,9 +1,12 @@
 package es.icarto.gvsig.commons.utils;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.PrintStream;
+import java.nio.channels.FileChannel;
 
 import org.apache.log4j.Logger;
 
@@ -22,7 +25,7 @@ public class FileUtils {
      *            to write
      * @return false if there was any trouble, so if (!
      *         FileUtils.write(contents, file)) { handle error }
-     * 
+     *
      */
     public static boolean write(String contents, File file) {
 	PrintStream ps = null;
@@ -37,6 +40,42 @@ public class FileUtils {
 	    }
 	}
 	return ps != null ? !ps.checkError() : false;
+    }
+
+    // http://stackoverflow.com/a/115086/930271
+
+    public static boolean copyFile(File sourceFile, File destFile) {
+
+	FileChannel source = null;
+	FileChannel destination = null;
+
+	try {
+	    if (!destFile.exists()) {
+		destFile.createNewFile();
+	    }
+	    source = new FileInputStream(sourceFile).getChannel();
+	    destination = new FileOutputStream(destFile).getChannel();
+	    destination.transferFrom(source, 0, source.size());
+	} catch (IOException e) {
+	    logger.error(e.getStackTrace(), e);
+	    return false;
+	} finally {
+	    if (source != null) {
+		try {
+		    source.close();
+		} catch (IOException e) {
+		    logger.error(e.getStackTrace(), e);
+		}
+	    }
+	    if (destination != null) {
+		try {
+		    destination.close();
+		} catch (IOException e) {
+		    logger.error(e.getStackTrace(), e);
+		}
+	    }
+	}
+	return true;
     }
 
 }
