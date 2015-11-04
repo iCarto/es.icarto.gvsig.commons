a9abd94610c07fcfd4a4e2f2ea2a717367f561c2
Francisco Puga <fpuga@icarto.es>
2015-11-04 12:06:04 +0100

    Adds a new widget to select folders

---
+++ src-test/es/icarto/gvsig/commons/gui/GenericGUITest.java
@@ -0,0 +1,23 @@
+package es.icarto.gvsig.commons.gui;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import net.miginfocom.swing.MigLayout;
+
+public class GenericGUITest {
+
+    public static void main(String[] args) {
+	JFrame f = new JFrame("Test");
+	f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	JPanel pane = new JPanel(new MigLayout("insets 10"));
+
+	f.getContentPane().add(pane, BorderLayout.CENTER);
+	f.pack();
+	f.setVisible(true);
+    }
+
+}
---
+++ src/es/icarto/gvsig/commons/gui/FolderChooser.java
@@ -0,0 +1,127 @@
+package es.icarto.gvsig.commons.gui;
+
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class FolderChooser {
+
+    private static final String prototypeDisplayValue = "XXXXXXXXXXXXXXXXXXXXXXX";
+
+    private JLabel label;
+    private final JPanel parent;
+    private final JTextField field;
+
+    private final JFileChooser fileChooser;
+
+    private final JButton bt;
+
+    public FolderChooser(JPanel parentCmp, String labelText, String initFile) {
+	this.parent = parentCmp;
+	if (labelText != null) {
+	    label = new JLabel(labelText);
+	    parent.add(label);
+	}
+
+	field = new JTextField();
+	field.setColumns(prototypeDisplayValue.length());
+	if (initFile != null) {
+	    field.setText(initFile);
+	}
+	parent.add(field); // grow
+	fileChooser = new JFileChooser();
+	fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+	fileChooser.setAcceptAllFileFilterUsed(false);
+	fileChooser.setFileFilter(new DirFileFilter());
+
+	bt = new JButton("...");
+	bt.addActionListener(new ActionListener() {
+	    @Override
+	    public void actionPerformed(ActionEvent e) {
+
+		fileChooser.setCurrentDirectory(new File(field.getText()));
+
+		// setCurrentDirectory(field.getText().getParentFile());
+		// setSelectedFile(field.getText);
+
+		// fc.setDialogType(JFileChooser.OPEN_DIALOG);
+		// fc.setDialogTitle(title);
+		// fc.showDialog(parent, "Abrir")
+
+		if (fileChooser.showOpenDialog(parent) == JFileChooser.APPROVE_OPTION) {
+		    File selectedFile = fileChooser.getSelectedFile();
+		    field.setText(selectedFile.getAbsolutePath());
+		}
+
+	    }
+	});
+
+	parent.add(bt);
+    }
+
+    public Component getDefaultFocusComponent() {
+	return field;
+    }
+
+    public void setToolTip2Components(String tooltip) {
+	if (label != null) {
+	    label.setToolTipText(tooltip);
+	}
+	field.setToolTipText(tooltip);
+	bt.setToolTipText(tooltip);
+    }
+
+    public boolean isFolder() {
+	File file = getFolder();
+	return fileChooser.accept(file);
+    }
+
+    public File getFolder() {
+	return new File(getFolderPath());
+    }
+
+    public String getFolderPath() {
+	String path = field.getText().trim();
+	return path.endsWith(File.separator) ? path : path + File.separator;
+    }
+
+    public void setEnabled(boolean enabled) {
+	label.setEnabled(enabled);
+	bt.setEnabled(enabled);
+	field.setEnabled(enabled);
+    }
+
+    public void setSelectedFile(String file) {
+	field.setText(file);
+    }
+
+    public class DirFileFilter extends javax.swing.filechooser.FileFilter {
+
+	private String description = "Directorios";
+
+	public DirFileFilter() {
+	}
+
+	public DirFileFilter(String description) {
+	    this.description = description;
+	}
+
+	@Override
+	public boolean accept(java.io.File f) {
+	    return f.isDirectory();
+	}
+
+	@Override
+	public String getDescription() {
+	    return description;
+	}
+    }
+
+}
