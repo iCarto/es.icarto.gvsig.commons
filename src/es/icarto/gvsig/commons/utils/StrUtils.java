f6f2f1ea2b2f0c38ad3c9ed7fd6d13533a7dbb3b
Francisco Puga <fpuga@icarto.es>
2016-06-01 19:09:25 +0200

    Adds a join method

--- src/es/icarto/gvsig/commons/utils/StrUtils.java
+++ src/es/icarto/gvsig/commons/utils/StrUtils.java
@@ -1,5 +1,6 @@
 package es.icarto.gvsig.commons.utils;
 
+
 public class StrUtils {
 
     private StrUtils() {
@@ -8,5 +9,17 @@ public class StrUtils {
 
     public static boolean isEmptyString(String str) {
 	return str == null || str.trim().length() == 0;
+    }
+
+    public static String join(String delimiter, Object... s) {
+	if ((s == null) || (s.length == 0)) {
+	    return "";
+	}
+	StringBuilder out = new StringBuilder();
+	out.append(s[0]);
+	for (int x = 1, l = s.length; x < l; ++x) {
+	    out.append(delimiter).append(s[x]);
+	}
+	return out.toString();
     }
 }
