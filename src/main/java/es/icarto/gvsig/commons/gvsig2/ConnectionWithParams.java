e2fcd17f3ead2f45575653d2eb95567dd3606187
Francisco Puga <fpuga@icarto.es>
2016-06-21 14:26:07 +0200

    Adds two new helper (Adapter) classes for gvsig2

---
+++ src/main/java/es/icarto/gvsig/commons/gvsig2/ConnectionWithParams.java
@@ -0,0 +1,115 @@
+package es.icarto.gvsig.commons.gvsig2;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import org.gvsig.fmap.dal.DALLocator;
+import org.gvsig.fmap.dal.DataServerExplorerParameters;
+import org.gvsig.fmap.dal.exception.InitializeException;
+import org.gvsig.fmap.dal.exception.ProviderNotRegisteredException;
+import org.gvsig.fmap.dal.resource.Resource;
+import org.gvsig.fmap.dal.resource.ResourceAction;
+import org.gvsig.fmap.dal.resource.exception.AccessResourceException;
+import org.gvsig.fmap.dal.resource.exception.ResourceExecuteException;
+import org.gvsig.fmap.dal.resource.spi.ResourceConsumer;
+import org.gvsig.fmap.dal.resource.spi.ResourceProvider;
+import org.gvsig.fmap.dal.spi.DataManagerProviderServices;
+import org.gvsig.fmap.dal.store.jdbc.JDBCResource;
+import org.gvsig.fmap.dal.store.jdbc.JDBCServerExplorerParameters;
+import org.gvsig.fmap.dal.store.jdbc.JDBCStoreParameters;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ConnectionWithParams implements ResourceConsumer {
+
+	
+	private static final Logger logger = LoggerFactory
+			.getLogger(ConnectionWithParams.class);
+	
+	private final DataServerExplorerParameters explorerParams;
+	private final String storeProviderName;
+	private final JDBCResource resource;
+	private final String name;
+
+	public ConnectionWithParams(DataServerExplorerParameters params, JDBCResource resource, String storeProviderName, String conName) {
+		this.explorerParams = params;
+		this.storeProviderName = storeProviderName;
+		this.resource = resource;
+		this.resource.addConsumer(this);
+		this.name = conName;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public Connection getConnection() {
+		try {
+			return resource.getJDBCConnection();
+		} catch (AccessResourceException e) {
+			logger.error(e.getMessage(), e);
+		}
+		return null;
+	}
+
+	public DataServerExplorerParameters getExplorerParams() {
+		return explorerParams;
+	}
+	
+	// Taken from JDBCServerExplorer.createStoreParams that can not be used as is a protected method
+	public JDBCStoreParameters getStoreParams()
+			throws InitializeException, ProviderNotRegisteredException {
+		DataManagerProviderServices manager = (DataManagerProviderServices) DALLocator.getDataManager();
+		
+		JDBCServerExplorerParameters parameters = (JDBCServerExplorerParameters) explorerParams;
+		JDBCStoreParameters orgParams = (JDBCStoreParameters) manager.createStoreParameters(storeProviderName);
+		orgParams.setHost(parameters.getHost());
+		orgParams.setPort(parameters.getPort());
+		orgParams.setDBName(parameters.getDBName());
+		orgParams.setUser(parameters.getUser());
+		orgParams.setPassword(parameters.getPassword());
+        orgParams.setCatalog(parameters.getCatalog());
+        orgParams.setJDBCDriverClassName(parameters.getJDBCDriverClassName());
+        orgParams.setSchema(parameters.getSchema());
+        orgParams.setUrl(parameters.getUrl());
+		return orgParams;
+	}
+	
+	
+	
+	public void close() {
+		try {
+			resource.execute(new ResourceAction() {
+				public Object run() throws Exception {
+					resource.notifyClose();
+					return null;
+				}
+			});
+			resource.getJDBCConnection().close();
+			resource.removeConsumer(this);
+		} catch (ResourceExecuteException e) {
+			logger.error(e.getMessage(), e);
+		} catch (AccessResourceException e) {
+			logger.error(e.getMessage(), e);
+		} catch (SQLException e) {
+			logger.error(e.getMessage(), e);
+		}
+	}
+
+	@Override
+	public boolean closeResourceRequested(ResourceProvider resource) {
+		return false;
+	}
+
+	@Override
+	public void resourceChanged(ResourceProvider resource) {
+	}
+
+	public Resource getResource() {
+		return resource;
+	}
+	
+	
+	
+
+}
---
+++ src/main/java/es/icarto/gvsig/commons/gvsig2/SingleDBConnectionManager.java
@@ -0,0 +1,142 @@
+package es.icarto.gvsig.commons.gvsig2;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.gvsig.fmap.dal.DALLocator;
+import org.gvsig.fmap.dal.DataManager;
+import org.gvsig.fmap.dal.DataParameters;
+import org.gvsig.fmap.dal.DataServerExplorerParameters;
+import org.gvsig.fmap.dal.DataServerExplorerPool;
+import org.gvsig.fmap.dal.DataServerExplorerPoolEntry;
+import org.gvsig.fmap.dal.exception.DataException;
+import org.gvsig.fmap.dal.resource.ResourceParameters;
+import org.gvsig.fmap.dal.resource.db.DBParameters;
+import org.gvsig.fmap.dal.resource.spi.ResourceManagerProviderServices;
+import org.gvsig.fmap.dal.resource.spi.ResourceProvider;
+import org.gvsig.fmap.dal.store.jdbc.JDBCResource;
+import org.gvsig.fmap.dal.store.jdbc.JDBCResourceParameters;
+import org.gvsig.fmap.dal.store.jdbc.JDBCServerExplorerParameters;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SingleDBConnectionManager {
+
+	private final Map<String, ConnectionWithParams> list = new HashMap<String, ConnectionWithParams>();
+	
+	private static final Logger logger = LoggerFactory
+			.getLogger(SingleDBConnectionManager.class);
+	
+	private final static SingleDBConnectionManager ins = new SingleDBConnectionManager();
+	
+	private SingleDBConnectionManager() {
+	}
+	
+	
+	public static SingleDBConnectionManager instance() {
+		return ins;
+	}
+
+
+	public void closeAndRemove(ConnectionWithParams conwp) {
+		ConnectionWithParams cwp = list.get(conwp.getName());
+		if (cwp != null) {
+			ResourceManagerProviderServices manager = (ResourceManagerProviderServices) DALLocator.getResourceManager();
+			try {
+				manager.remove(cwp.getResource());
+			} catch (DataException e) {
+				logger.error(e.getMessage(), e);
+			}
+			cwp.close();
+			
+			DataManager dataManager = DALLocator.getDataManager();
+			DataServerExplorerPool pool = dataManager.getDataServerExplorerPool();
+			pool.remove(conwp.getName());
+		}
+	}
+
+
+	/**
+	 * El identificador de conexiones es "conName" a todos los efectos
+	 * Crea un nuevo DataServerExplorerParameters al pool si es necesario (Para usar en el panel de conexión a base de datos)
+	 */
+	public ConnectionWithParams getConnection(String storeProviderName, String serverExplorerName, String resourceName,
+			String username, String password, String conName, String server,
+			int port, String database, String schema, boolean connect) throws DataException {
+		
+		ConnectionWithParams cwp = list.get(conName);
+		if (cwp != null) {
+			return cwp;
+		}
+		JDBCServerExplorerParameters serverExplorerParams = createServerExplorerParams(conName, serverExplorerName, server, port, database, username, password, schema);
+		JDBCResource resource = createResource(serverExplorerParams, resourceName);
+				
+		return new ConnectionWithParams(serverExplorerParams, resource, storeProviderName, conName);
+	}
+	
+	private JDBCServerExplorerParameters createServerExplorerParams(String conName, String serverExplorerName, String server,
+			int port, String database, String username, String password,
+			String schema) throws DataException {
+		DataManager dataManager = DALLocator.getDataManager();
+		
+				
+		DataServerExplorerPool pool = dataManager.getDataServerExplorerPool();
+		DataServerExplorerPoolEntry entry = pool.get(conName);
+		
+		DataServerExplorerParameters params = null;
+		if (entry != null) {
+			 params = entry.getExplorerParameters();
+		}
+		if (params != null) {
+			return (JDBCServerExplorerParameters) params;
+		} else {
+			params = dataManager.createServerExplorerParameters(serverExplorerName);
+			params.setDynValue(DBParameters.HOST_PARAMTER_NAME, server);
+			params.setDynValue(DBParameters.PORT_PARAMTER_NAME, port);
+			params.setDynValue(DBParameters.DBNAME_PARAMTER_NAME, database);
+			params.setDynValue(DBParameters.USER_PARAMTER_NAME, username);
+			params.setDynValue(DBParameters.PASSWORD_PARAMTER_NAME, password);
+			if ((schema != null) && (!schema.isEmpty())) {
+				params.setDynValue("schema", schema);			
+			}
+			pool.add(conName, params);
+		}
+		return (JDBCServerExplorerParameters) params;
+	}
+
+
+	private JDBCResource createResource(JDBCServerExplorerParameters params, String resourceName) throws DataException {
+		
+//		Class klass = null;
+//		try {
+//			klass = Class.forName("org.postgresql.Driver");
+//		} catch (ClassNotFoundException e) {
+//			logger.error(e.getMessage(), e);
+//			System.out.println("*************************** NOT FOUND ********************");
+//			System.out.println("*************************** NOT FOUND ********************");
+//			System.out.println("*************************** NOT FOUND ********************");
+//			System.out.println("*************************** NOT FOUND ********************");
+//		}
+//		if (klass == null) {
+//			System.out.println("*************************** NOT FOUND ********************");
+//			System.out.println("*************************** NOT FOUND ********************");
+//			System.out.println("*************************** NOT FOUND ********************");
+//			System.out.println("*************************** NOT FOUND ********************");
+//		}
+
+		ResourceManagerProviderServices manager = (ResourceManagerProviderServices) DALLocator.getResourceManager();
+		ResourceParameters resourceParams = (ResourceParameters) manager.createParameters(resourceName);
+//		resourceParams.setDynValue(JDBCResourceParameters.URL_PARAMTER_NAME, params.getUrl());
+		resourceParams.setDynValue(JDBCResourceParameters.HOST_PARAMTER_NAME, params.getHost());
+		resourceParams.setDynValue(JDBCResourceParameters.PORT_PARAMTER_NAME, params.getPort());
+		resourceParams.setDynValue(JDBCResourceParameters.DBNAME_PARAMTER_NAME, params.getDBName());
+		resourceParams.setDynValue(JDBCResourceParameters.USER_PARAMTER_NAME, params.getUser());
+		resourceParams.setDynValue(JDBCResourceParameters.PASSWORD_PARAMTER_NAME, params.getPassword());
+		resourceParams.setDynValue(JDBCResourceParameters.JDBC_DRIVER_CLASS_PARAMTER_NAME, params.getJDBCDriverClassName());
+		ResourceProvider resource = manager.createAddResource(resourceParams);
+		
+		return (JDBCResource) resource;
+		
+	}
+
+}
