482db452606d93fd9e1f32aeb8e89e2a43224084
Francisco Puga <fpuga@icarto.es>
2016-07-04 21:25:49 +0200

    Avoid nullpointerexception when there are no layers to choose

--- src/main/java/es/icarto/gvsig/commons/gui/ChooseLayerPanel.java
+++ src/main/java/es/icarto/gvsig/commons/gui/ChooseLayerPanel.java
@@ -13,164 +13,163 @@ import org.gvsig.app.project.documents.view.gui.IView;
 import org.gvsig.fmap.mapcontext.layers.FLayer;
 
 @SuppressWarnings("serial")
-/**
- * The api of this class will change in the future. Comment that you are going
- * to use
- */
-@Deprecated
 public class ChooseLayerPanel<T extends FLayer> extends JPanel {
 
-    public enum Orientation {
-	VERTICAL, HORIZONTAL, PLACEHOLDER, NONE
-    }
+	public enum Orientation {
+		VERTICAL, HORIZONTAL, PLACEHOLDER, NONE
+	}
 
-    private final LayerItem EMPTY = new LayerItem(null);
+	private final LayerItem EMPTY = new LayerItem(null);
 
-    private final class LayerItem {
+	private final class LayerItem {
+
+		/**
+		 * FLayer classes overrides toString method for some opaque reason, so
+		 * we can not put it directly on a combo This is a wrapper
+		 */
+		private final T layer;
+		private final String name;
+
+		public LayerItem(T layer) {
+			this.layer = layer;
+			this.name = layer != null ? layer.getName() : "";
+		}
+
+		public T getLayer() {
+			return this.layer;
+		}
+
+		@Override
+		public String toString() {
+			return this.name;
+		}
+	}
+
+	private final JComboBox layerCombo;
+
+	private final Class<T> clasz;
+
+	private final JPanel parentComp;
+
+	private static final String prototypeDisplayValue = "XXXXXXXXXXXXXXXXXXXX";
 
 	/**
-	 * FLayer classes overrides toString method for some opaque reason, so
-	 * we can not put it directly on a combo This is a wrapper
+	 * @text the text to use for the label. If null or empty Orientation.NONE
+	 *       will be assumed. The text will be automatically i18n through
+	 *       PluginServices
+	 *
+	 * @param orientation
+	 *            if the label of the combo should be in the left of the combo
+	 *            (HORIZONTAL), over the combo (VERTICAL) or instead of use a
+	 *            label a placeholder should be used in the combo itself
+	 *            (PLACEHOLDER), or even with no label at all (NONE)
 	 */
-	private final T layer;
-	private final String name;
+	public ChooseLayerPanel(JPanel parentComp, String text,
+			Orientation orientation, Class<T> clasz) {
 
-	public LayerItem(T layer) {
-	    this.layer = layer;
-	    this.name = layer != null ? layer.getName() : "";
+		this.clasz = clasz;
+		this.parentComp = parentComp;
+
+		JLabel label = null;
+		if ((text == null) || text.isEmpty()) {
+			orientation = Orientation.NONE;
+		} else {
+			label = new JLabel(PluginServices.getText(this, text));
+		}
+
+		layerCombo = new JComboBox();
+		layerCombo.setPrototypeDisplayValue(prototypeDisplayValue);
+
+		switch (orientation) {
+		case HORIZONTAL:
+			parentComp.add(label);
+			parentComp.add(layerCombo, "wrap, growx");
+			break;
+
+		default:
+			throw new AssertionError("Not implemented jet");
+		}
+
 	}
 
-	public T getLayer() {
-	    return this.layer;
+	/*
+	 * Preselects in the combo the first layer active of type T and returns it
+	 * or null if there is no active layer
+	 */
+	public T preselectFirstActive() {
+		for (int i = 0; i < layerCombo.getItemCount(); i++) {
+			LayerItem itemAt = (LayerItem) layerCombo.getItemAt(i);
+
+			T l = itemAt.getLayer();
+			if ((l != null) && l.isActive()) {
+				layerCombo.setSelectedIndex(i);
+				return l;
+			}
+		}
+		return null;
 	}
 
-	@Override
-	public String toString() {
-	    return this.name;
-	}
-    }
-
-    private final JComboBox layerCombo;
-
-    private final Class<T> clasz;
-
-    private final JPanel parentComp;
-
-    private static final String prototypeDisplayValue = "XXXXXXXXXXXXXXXXXXXX";
-
-    /**
-     * @text the text to use for the label. If null or empty Orientation.NONE
-     *       will be assumed. The text will be automatically i18n through
-     *       PluginServices
-     *
-     * @param orientation
-     *            if the label of the combo should be in the left of the combo
-     *            (HORIZONTAL), over the combo (VERTICAL) or instead of use a
-     *            label a placeholder should be used in the combo itself
-     *            (PLACEHOLDER), or even with no label at all (NONE)
-     */
-    public ChooseLayerPanel(JPanel parentComp, String text,
-	    Orientation orientation, Class<T> clasz) {
-
-	this.clasz = clasz;
-	this.parentComp = parentComp;
-
-	JLabel label = null;
-	if ((text == null) || text.isEmpty()) {
-	    orientation = Orientation.NONE;
-	} else {
-	    label = new JLabel(PluginServices.getText(this, text));
+	/**
+	 * Adds an empty first item in the combo and preselects it [preselect] is
+	 * true Take care with the order in which preselectFirstActive and this
+	 * method is used
+	 *
+	 * Should be called before populateFrom
+	 *
+	 * @param preselect
+	 */
+	public void addEmptyFirst(boolean preselect) {
+		layerCombo.addItem(EMPTY);
 	}
 
-	layerCombo = new JComboBox();
-	layerCombo.setPrototypeDisplayValue(prototypeDisplayValue);
-
-	switch (orientation) {
-	case HORIZONTAL:
-	    parentComp.add(label);
-	    parentComp.add(layerCombo, "wrap, growx");
-	    break;
-
-	default:
-	    throw new AssertionError("Not implemented jet");
+	/**
+	 * Returns the selected layer or null if none is selected
+	 */
+	public T getSelected() {
+		LayerItem sel = (LayerItem) layerCombo.getSelectedItem();
+		return sel != null ? sel.getLayer() : null;
 	}
 
-    }
+	/**
+	 * Returns a list with the layers used to fill the combo
+	 *
+	 * @param filter
+	 *            can be null to populate with all layers of the class defined
+	 *            in the constructor
+	 */
+	public List<T> populateFrom(IView view, LayerFilter filter) {
+		TOCLayerManager toc = new TOCLayerManager(view.getMapControl());
+		List<T> allLayers = toc.getAllLayers(clasz);
 
-    /*
-     * Preselects in the combo the first layer active of type T and returns it
-     * or null if there is no active layer
-     */
-    public T preselectFirstActive() {
-	for (int i = 0; i < layerCombo.getItemCount(); i++) {
-	    LayerItem itemAt = (LayerItem) layerCombo.getItemAt(i);
-
-	    T l = itemAt.getLayer();
-	    if ((l != null) && l.isActive()) {
-		layerCombo.setSelectedIndex(i);
-		return l;
-	    }
+		for (T t : allLayers) {
+			if ((filter == null) || (filter.accept(t))) {
+				layerCombo.addItem(new LayerItem(t));
+			}
+		}
+		return allLayers;
 	}
-	return null;
-    }
 
-    /**
-     * Adds an empty first item in the combo and preselects it [preselect] is
-     * true Take care with the order in which preselectFirstActive and this
-     * method is used
-     *
-     * Should be called before populateFrom
-     *
-     * @param preselect
-     */
-    public void addEmptyFirst(boolean preselect) {
-	layerCombo.addItem(EMPTY);
-    }
-
-    /**
-     * Returns the selected layer or null if none is selected
-     */
-    public T getSelected() {
-	LayerItem sel = (LayerItem) layerCombo.getSelectedItem();
-	return sel.getLayer();
-    }
-
-    /**
-     * Returns a list with the layers used to fill the combo
-     */
-    public List<T> populateFrom(IView view, LayerFilter filter) {
-	TOCLayerManager toc = new TOCLayerManager(view.getMapControl());
-	List<T> allLayers = toc.getAllLayers(clasz);
-
-	for (T t : allLayers) {
-	    if ((filter == null) || (filter.accept(t))) {
-		layerCombo.addItem(new LayerItem(t));
-	    }
+	public List<T> populateFromActiveView() {
+		return populateFromActiveView(null);
 	}
-	return allLayers;
-    }
 
-    public List<T> populateFromActiveView() {
-	return populateFromActiveView(null);
-    }
-
-    private IView getView() {
-	IWindow iWindow = PluginServices.getMDIManager().getActiveWindow();
-	if (iWindow instanceof IView) {
-	    return (IView) iWindow;
+	private IView getView() {
+		IWindow iWindow = PluginServices.getMDIManager().getActiveWindow();
+		if (iWindow instanceof IView) {
+			return (IView) iWindow;
+		}
+		return null;
 	}
-	return null;
-    }
 
-    public List<T> populateFromActiveView(LayerFilter filter) {
-	IView view = getView();
-	if (view != null) {
-	    return populateFrom(view, filter);
+	public List<T> populateFromActiveView(LayerFilter filter) {
+		IView view = getView();
+		if (view != null) {
+			return populateFrom(view, filter);
+		}
+		return null;
 	}
-	return null;
-    }
 
-    public Component getDefaultFocusComponent() {
-	return layerCombo;
-    }
+	public Component getDefaultFocusComponent() {
+		return layerCombo;
+	}
 }
