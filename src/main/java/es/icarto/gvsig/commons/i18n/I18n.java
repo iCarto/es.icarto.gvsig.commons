c2c9d030c7d98190c3b52090ce06e2d21ff5a21d
Francisco Puga <fpuga@icarto.es>
2016-07-06 10:44:10 +0200

    Adds i18n utilities

---
+++ src/main/java/es/icarto/gvsig/commons/i18n/I18n.java
@@ -0,0 +1,49 @@
+package es.icarto.gvsig.commons.i18n;
+
+import java.text.MessageFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import org.gvsig.i18n.Messages;
+
+public class I18n {
+
+	private final static MessageFormat formatter = new MessageFormat("",
+			Messages.getCurrentLocale());
+	private final static boolean LOG_FAILED_TRANSLATIONS = false;
+
+	public static String _(String text) {
+		return Messages.getText(text, LOG_FAILED_TRANSLATIONS);
+	}
+
+	public static String _(String text, Object[] args) {
+		String pattern = Messages.getText(text, LOG_FAILED_TRANSLATIONS);
+		if (args != null) {
+			formatter.applyPattern(pattern);
+			return formatter.format(args);
+		}
+		return pattern;
+	}
+
+	private static void foo() {
+
+		Locale currentLocale = Locale.getDefault(); // cogerlo de gvSIG
+		ResourceBundle messages = ResourceBundle.getBundle("MessageBundle",
+				currentLocale);
+
+		Object[] messageArguments = { messages.getString("planet"),
+				new Integer(7), new Date() };
+		MessageFormat formatter = new MessageFormat("");
+		formatter.setLocale(currentLocale);
+
+		formatter.applyPattern(messages.getString("template"));
+		String output = formatter.format(messageArguments);
+	}
+
+	public static List getNotTranslatedKeys() {
+		return Messages.getNotTranslatedKeys();
+	}
+
+}
