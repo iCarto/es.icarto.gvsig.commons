3043810497ababc3a2722c9bcb564cb00c66edcd
Francisco Puga <fpuga@icarto.es>
2019-10-08 13:42:29 +0200

    Adds method isViewActive to AbstractExtension

--- src/main/java/es/icarto/gvsig/commons/AbstractExtension.java
+++ src/main/java/es/icarto/gvsig/commons/AbstractExtension.java
@@ -8,31 +8,39 @@ import org.gvsig.app.project.documents.view.gui.IView;
 
 public abstract class AbstractExtension extends Extension {
 
-    public final String id = this.getClass().getName();
+	public final String id = this.getClass().getName();
 
-    private void registerIcon(String iconName) {
-	IconThemeHelper.registerIcon("action", iconName, this);
-    }
-
-    @Override
-    public void initialize() {
-	registerIcon(id);
-    }
-
-    @Override
-    public boolean isVisible() {
-	return true;
-    }
-
-    /**
-     * Returns the active window if is a View. Returns null elsewhere
-     */
-    protected IView getView() {
-	IWindow iWindow = PluginServices.getMDIManager().getActiveWindow();
-	if (iWindow instanceof IView) {
-	    return (IView) iWindow;
+	private void registerIcon(String iconName) {
+		IconThemeHelper.registerIcon("action", iconName, this);
 	}
-	return null;
-    }
+
+	@Override
+	public void initialize() {
+		registerIcon(id);
+	}
+
+	@Override
+	public boolean isVisible() {
+		return true;
+	}
+
+	/**
+	 * Returns the active window if is a View. Returns null elsewhere
+	 */
+	protected IView getView() {
+		IWindow iWindow = PluginServices.getMDIManager().getActiveWindow();
+		if (iWindow instanceof IView) {
+			return (IView) iWindow;
+		}
+		return null;
+	}
+
+	/**
+	 *
+	 * @return true if the active window is a View
+	 */
+	protected boolean isViewActive() {
+		return getView() != null;
+	}
 
 }
