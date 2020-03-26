a95ee3faab51da8d8fcc8aa36001a0f891dfb56d
Francisco Puga <fpuga@icarto.es>
2020-03-26 14:11:03 +0100

    Allows to use bigint in FieldDescriptionFactory
    refs #1770

--- src/es/icarto/gvsig/commons/datasources/FieldDescriptionFactory.java
+++ src/es/icarto/gvsig/commons/datasources/FieldDescriptionFactory.java
@@ -15,90 +15,105 @@ public class FieldDescriptionFactory {
     private final List<FieldDescription> fields = new ArrayList<FieldDescription>();
 
     public void setDefaultStringLength(int defaultStringLength) {
-	this.stringLength = defaultStringLength;
+        this.stringLength = defaultStringLength;
     }
 
     public void setDefaultNumericLength(int defaultNumericLength) {
-	this.numericLength = defaultNumericLength;
+        this.numericLength = defaultNumericLength;
     }
 
     public void setDefaultDecimalCount(int defaultDecimalCount) {
-	this.decimalCount = defaultDecimalCount;
+        this.decimalCount = defaultDecimalCount;
     }
 
     public FieldDescription addInteger(String name) {
-	FieldDescription fd = getInteger(name);
-	fields.add(fd);
-	return fd;
+        FieldDescription fd = getInteger(name);
+        fields.add(fd);
+        return fd;
+    }
+
+    public FieldDescription addBigInteger(String name) {
+        FieldDescription fd = getInteger(name);
+        fields.add(fd);
+        return fd;
     }
 
     public FieldDescription getInteger(String name) {
-	FieldDescription fd = new FieldDescription();
-	fd.setFieldName(name);
-	fd.setFieldLength(numericLength);
-	fd.setFieldDecimalCount(0);
-	fd.setFieldType(Types.INTEGER);
-	return fd;
+        FieldDescription fd = new FieldDescription();
+        fd.setFieldName(name);
+        fd.setFieldLength(numericLength);
+        fd.setFieldDecimalCount(0);
+        fd.setFieldType(Types.INTEGER);
+        return fd;
+    }
+
+    public FieldDescription getBigInteger(String name) {
+        FieldDescription fd = new FieldDescription();
+        fd.setFieldName(name);
+        fd.setFieldLength(numericLength);
+        fd.setFieldDecimalCount(0);
+        fd.setFieldType(Types.BIGINT);
+        return fd;
     }
 
     public FieldDescription addDouble(String name) {
-	FieldDescription fd = getDouble(name);
-	fields.add(fd);
-	return fd;
+        FieldDescription fd = getDouble(name);
+        fields.add(fd);
+        return fd;
     }
 
     public FieldDescription getDouble(String name) {
-	FieldDescription fd = new FieldDescription();
-	fd.setFieldName(name);
-	fd.setFieldLength(numericLength);
-	fd.setFieldDecimalCount(decimalCount);
-	fd.setFieldType(Types.DOUBLE);
-	return fd;
+        FieldDescription fd = new FieldDescription();
+        fd.setFieldName(name);
+        fd.setFieldLength(numericLength);
+        fd.setFieldDecimalCount(decimalCount);
+        fd.setFieldType(Types.DOUBLE);
+        return fd;
     }
 
     public FieldDescription addString(String name) {
-	FieldDescription fd = getString(name);
-	fields.add(fd);
-	return fd;
+        FieldDescription fd = getString(name);
+        fields.add(fd);
+        return fd;
     }
 
     public FieldDescription getString(String name) {
-	FieldDescription fd = new FieldDescription();
-	fd.setFieldName(name);
-	fd.setFieldLength(stringLength);
-	fd.setFieldDecimalCount(0);
-	fd.setFieldType(Types.VARCHAR);
-	return fd;
+        FieldDescription fd = new FieldDescription();
+        fd.setFieldName(name);
+        fd.setFieldLength(stringLength);
+        fd.setFieldDecimalCount(0);
+        fd.setFieldType(Types.VARCHAR);
+        return fd;
     }
 
     public FieldDescription addDate(String name) {
-	FieldDescription fd = getDate(name);
-	fields.add(fd);
-	return fd;
+        FieldDescription fd = getDate(name);
+        fields.add(fd);
+        return fd;
     }
 
     public FieldDescription getDate(String name) {
-	FieldDescription fd = new FieldDescription();
-	fd.setFieldName(name);
-	fd.setFieldType(Types.DATE);
-	return fd;
+        FieldDescription fd = new FieldDescription();
+        fd.setFieldName(name);
+        fd.setFieldType(Types.DATE);
+        return fd;
     }
 
     public FieldDescription addBoolean(String name) {
-	FieldDescription fd = getBoolean(name);
-	fields.add(fd);
-	return fd;
+        FieldDescription fd = getBoolean(name);
+        fields.add(fd);
+        return fd;
     }
 
     public FieldDescription getBoolean(String name) {
-	FieldDescription fd = new FieldDescription();
-	fd.setFieldName(name);
-	fd.setFieldType(Types.BIT);
-	return fd;
+        FieldDescription fd = new FieldDescription();
+        fd.setFieldName(name);
+        fd.setFieldType(Types.BIT);
+        return fd;
     }
 
     public FieldDescription[] getFields() {
-	return fields.toArray(new FieldDescription[0]);
+        return fields.toArray(new FieldDescription[0]);
     }
 
 }
