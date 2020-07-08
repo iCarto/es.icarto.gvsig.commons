1aa135c81a1402a6b89549d12370489f0382b72f
Pablo Sanxiao <psanxiao@icarto.es>
2020-07-08 13:54:04 +0200

    Make matcher method public

--- src/es/icarto/gvsig/commons/legend/FieldLabelRE.java
+++ src/es/icarto/gvsig/commons/legend/FieldLabelRE.java
@@ -8,21 +8,21 @@ public class FieldLabelRE {
     private final Pattern p = Pattern.compile("\\[(\\w+)\\]");
 
     public String limitTo10(String s) {
-        Matcher m = p.matcher(s);
+	Matcher m = p.matcher(s);
 
-        StringBuffer sb = new StringBuffer();
-        while (m.find()) {
-            String replacement = m.group(1);
-            int l = Math.min(replacement.length(), 10);
-            replacement = "[" + replacement.substring(0, l) + "]";
-            m.appendReplacement(sb, replacement);
-        }
-        m.appendTail(sb);
-        return sb.toString();
+	StringBuffer sb = new StringBuffer();
+	while (m.find()) {
+	    String replacement = m.group(1);
+	    int l = Math.min(replacement.length(), 10);
+	    replacement = "[" + replacement.substring(0, l) + "]";
+	    m.appendReplacement(sb, replacement);
+	}
+	m.appendTail(sb);
+	return sb.toString();
     }
 
-    Matcher matcher(String s) {
-        return p.matcher(s);
+    public Matcher matcher(String s) {
+	return p.matcher(s);
     }
 
 }
